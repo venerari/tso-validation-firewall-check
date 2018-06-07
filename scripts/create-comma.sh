9a839fbc05e9189432c0e52d6eb8283311d9c173
venerari <venerayan@gmail.com>
2018-06-07 14:41:52 -0400

    9

--- scripts/create-comma.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-rm -f comma.csv
-n=$(cat windows.csv | wc -l)
-for (( c=1; c<=n; c++)) ; do echo ",">> comma.csv ; done
--- scripts/upper.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python
-import os, sys
-with open(sys.argv[1], "r+b") as file:
-    content = file.read()
-    file.seek(0)
-    file.write(content.upper())
